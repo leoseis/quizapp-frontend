702b4ca4c16fac489bae714c786354d86b123c62
leoseis <eboscrib2@yahoo.com>
2025-01-27 09:36:57 +0100

    added post request and studentusername username

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import React from 'react';
+import api from '../api';
 
-const StartScreen = ({numQuestions,username,setUsername}) => {
+const StartScreen = ({ numQuestions, username, setUsername }) => {
+  const studentUsername = { username: username };
+
+  function submitUsername() {
+    api.post("has_taken_quiz/", studentUsername)
+      .then(res => {
+        console.log(res.data); 
+      })
+      .catch(err => {
+        console.error("Error:", err.message); 
+      });
+  }
+
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
-      <h3>{numQuestions}  Questions to test your React mastery</h3>
-      {/* {error && <Error error={error} />} */}
+      <h3>{numQuestions} Questions to test your React mastery</h3>
       <input
         placeholder="Enter username"
-        className="btn btn-ui"
+        className="input-ui"
         style={{ marginBottom: "20px", textTransform: "uppercase" }}
         value={username}
-        onChange={(e)=> setUsername(e.target.value)}
+        onChange={(e) => setUsername(e.target.value)}
       />
-      <button className="btn btn-ui" >  Let's start </button>
+      <button
+        className="btn btn-ui"
+        onClick={submitUsername}
+        disabled={!username || username.trim() === ""}
+      >
+        Let's start
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default StartScreen
+export default StartScreen;
