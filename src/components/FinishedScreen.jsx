f99dccd39d80f9f3d2a6e0961675de24ddfcd3e6
leoseis <eboscrib2@yahoo.com>
2025-04-08 12:57:22 +0100

    addin % to the finishedscreen componeny

--- src/components/FinishedScreen.jsx
+++ src/components/FinishedScreen.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const FinishedScreen = ({studentScore,username,quizTotalScore}) => {
-
+const pecentage =(studentScore/quizTotalScore)* 100
   return (
     <>
       <p className="result">
@@ -9,7 +9,7 @@ const FinishedScreen = ({studentScore,username,quizTotalScore}) => {
         <span style={{ textTransform: "uppercase" }}>{username}</span>, you
         scored <strong>{studentScore}</strong> out of {quizTotalScore} 
       </p>
-      <p className="highscore">(HighScore: {studentScore} points)</p>
+      <p className="highscore">(HighScore: {studentScore} points) ({pecentage}%)</p>
 
       <button className=" btn btn-ui" >Restart Quiz</button>
     </>
