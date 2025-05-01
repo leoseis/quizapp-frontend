5f342a01f3c3e528f9bafd935e459d586b27c6f0
leoseis <eboscrib2@yahoo.com>
2025-05-01 11:58:33 +0100

    addded 2 digits to secs

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -18,7 +18,7 @@ const Timer = ({timeRemaining,  setTimeRemaining,setLoadingState}) => {
   }
   
   return (
-    <div className='timer'>{mins < 10 && "0"}{mins}:{secs }</div>
+    <div className='timer'>{mins < 10 && "0"}{mins}: {secs < 10 && "0"}{secs }</div>
   )
 }
 
